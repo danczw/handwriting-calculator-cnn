831974aac46772d6f5df4bd934eb9c0b795fd59a
Daniel <dev.dc@pm.me>
2021-09-13 16:54:11 +0200

    [feat] cnn architecture definition and training

--- calculatorCNN.py
+++ calculatorCNN.py
@@ -26,3 +26,52 @@ X_test = X_test.astype('float32') / 255
 # translate to categorical labels
 y_train_cat = keras.utils.to_categorical(y_train)
 y_test_cat = keras.utils.to_categorical(y_test)
+
+#------------------------------------------------------------------------------#
+#                         Convolutional Neural Network                         #
+#------------------------------------------------------------------------------#
+
+# set model parameter
+img_size = (img_w, img_h, 1)
+pool_size = (2, 2)
+kernel_tupel = (3, 3)
+dropout_ratio = 0.25
+
+# define mode
+model = keras.models.Sequential()
+
+model.add(layers.Conv2D(32, kernel_tupel, activation='relu', input_shape=img_size))
+model.add(layers.MaxPooling2D(pool_size))
+model.add(layers.Dropout(dropout_ratio))
+
+model.add(layers.Conv2D(64, kernel_tupel, activation='relu', input_shape=img_size))
+model.add(layers.MaxPooling2D(pool_size))
+model.add(layers.Dropout(dropout_ratio))
+
+model.add(layers.Conv2D(128, kernel_tupel, activation='relu', input_shape=img_size))
+model.add(layers.MaxPooling2D(pool_size))
+model.add(layers.Dropout(dropout_ratio))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+print(model.summary())
+
+# fit model
+epochs = 5
+batch = 32
+val_split = 0.3
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+history_test = model.fit(X_train, y_train_cat,
+    epochs=epochs,
+    verbose=1, 
+    validation_split=val_split,
+    batch_size=batch
+)
+
+# show test accuracy
+score = model.evaluate(X_test, y_test_cat, verbose=0)
+print('Test accuracy:', score[1])
